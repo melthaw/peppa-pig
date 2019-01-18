07c49a2a220cd737d937b0496a4a5cdba3962922
chenxue <melthaw@gmail.com>
2019-01-18 17:26:35 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # peppa-pig
+
 Draw a peppa pig using pythong
+
+
+
+
+
+# Get Started
+
+* Install dependencies
+
+```
+pip3 install -r requirements.txt
+```
+
+* Run it
+
+```
+python main.py
+```
+
---
+++ main.py
@@ -0,0 +1,162 @@
+from turtle import *
+
+def nose(x, y):
+    penup()
+    goto(x, y)
+    pendown()
+    setheading(-30)
+    begin_fill()
+    a = 0.4
+    for i in range(120):
+        if 0 <= i < 30 or 60 <= i < 90:
+            a = a + 0.08
+            left(3)
+            forward(a)
+        else:
+            a = a - 0.08
+            left(3)
+            forward(a)
+    end_fill()
+
+    penup()
+    setheading(90)
+    forward(25)
+    setheading(0)
+    forward(10)
+    pendown()
+    pencolor(255, 155, 192)
+    setheading(10)
+    begin_fill()
+    circle(5)
+    color(160, 82, 45)
+    end_fill()
+
+    penup()
+    setheading(0)
+    forward(20)
+    pendown()
+    pencolor(255, 155, 192)
+    setheading(10)
+    begin_fill()
+    circle(5)
+    color(160, 82, 45)
+    end_fill()
+
+
+def head(x, y):
+    color((255, 155, 192), "pink")
+    penup()
+    goto(x, y)
+    setheading(0)
+    pendown()
+    begin_fill()
+    setheading(180)
+    circle(300, -30)
+    circle(100, -60)
+    circle(80, -100)
+    circle(150, -20)
+    circle(60, -95)
+    setheading(161)
+    circle(-300, 15)
+    penup()
+    goto(-100, 100)
+    pendown()
+    setheading(-30)
+    a = 0.4
+    for i in range(60):
+        if 0 <= i < 30 or 60 <= i < 90:
+            a = a + 0.08
+            lt(3)
+            fd(a)
+        else:
+            a = a - 0.08
+            lt(3)
+            fd(a)
+    end_fill()
+
+
+def ears(x, y):
+    color((255, 155, 192), "pink")
+    penup()
+    goto(x, y)
+    pendown()
+    begin_fill()
+    setheading(100)
+    circle(-50, 50)
+    circle(-10, 120)
+    circle(-50, 54)
+    end_fill()
+    penup()
+    setheading(90)
+    forward(-12)
+    setheading(0)
+    forward(30)
+    pendown()
+    begin_fill()
+    setheading(100)
+    circle(-50, 50)
+    circle(-10, 120)
+    circle(-50, 56)
+    end_fill()
+
+def eyes(x, y):
+    penup()
+    goto(x, y)
+
+    penup()
+    setheading(90)
+    forward(25)
+    setheading(0)
+    forward(10)
+    pendown()
+    pencolor(255, 155, 192)
+    setheading(10)
+    begin_fill()
+    circle(10)
+    color(160, 82, 45)
+    end_fill()
+
+def cheek(x, y):
+    color((255, 155, 192))
+    penup()
+    goto(x, y)
+    pendown()
+    setheading(0)
+    begin_fill()
+    circle(30)
+    end_fill()
+
+
+def mouth(x, y):
+    color(239, 69, 19)
+    penup()
+    goto(x, y)
+    pendown()
+    setheading(-80)
+    circle(30, 40)
+    circle(40, 80)
+
+
+def setting():
+    pensize(4)
+    hideturtle()
+    colormode(255)
+    color((255, 155, 192), "pink")
+    setup(840, 500)
+    speed(10)
+
+
+def main():
+    setting()
+    nose(-100, 100)
+    head(-69, 167)
+    ears(0, 160)
+    eyes(-10, 70)
+    eyes(30, 50)
+    cheek(80, 10)
+    mouth(-20, 30)
+    done()
+
+
+if __name__ == '__main__':
+    main()
